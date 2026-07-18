b2833fdc14b6c0148822a42e7ad2b64e9182f1f5
Onur Solmaz <2453968+osolmaz@users.noreply.github.com>
2026-07-19 00:53:58 +0800

    perf(bench): increase Inkling concurrency

--- tests/test_bench_run.py
+++ tests/test_bench_run.py
@@ -153,6 +153,7 @@ class TestRegistry:
         inkling = load_profile("inkling-max")
         assert inkling.requested_model == "thinkingmachines/Inkling:together"
         assert inkling.generate["reasoning_effort"] == "max"
+        assert inkling.max_connections == 32
 
 
 class TestCost:
